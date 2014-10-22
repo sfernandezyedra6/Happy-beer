0d26b1b3b58b82cf9514c2b26b430e728c1f779a
Samuel Fernández <sfernandezyedra6@gmail.com>
2014-10-22 18:53:49 +0200

    "Level 1"

--- Happy/Assets/Scripts/scoreUp.cs
+++ Happy/Assets/Scripts/scoreUp.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class scoreUp : MonoBehaviour {
 
+	private Text texto;
+
+
+	void Start () {
+		texto = GameObject.Find ("Text Score").GetComponent<Text>();
+	}
 	void OnTriggerEnter2D(Collider2D col){
 		GameControl.score = GameControl.score + 1;
-		Debug.Log (GameControl.score);
-	
+		//Debug.Log (GameControl.score);
+		
+			texto.text  = "Level " + GameControl.score.ToString ();
 	}
 }
